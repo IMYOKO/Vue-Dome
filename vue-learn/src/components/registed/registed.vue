170066888ad8d74802f9f7ef934fb9fe191898be
YOKO <752373772@qq.com>
2018-03-30 22:40:36 +0800

    add  author

--- vue-learn/src/components/registed/registed.vue
+++ vue-learn/src/components/registed/registed.vue
@@ -1,3 +1,9 @@
+/*
+ * @Author: YOKO 
+ * @Date: 2018-03-30 22:25:48 
+ * @Last Modified by: YOKO
+ * @Last Modified time: 2018-03-30 22:36:24
+ */
 <template>
   <div class="registed">
     注册
@@ -6,7 +12,14 @@
 
 <script>
 export default {
-  
+  data() {
+    
+  },
+  mounted:{
+    comont() {
+      console.log('(123())');
+    }
+  }
 }
 </script>
 
